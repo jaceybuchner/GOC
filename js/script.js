dc993304c1c6a6c0161b810ab0a4d092968d48d9
Jacey Buchner <jjmb.private@gmail.com>
2020-07-22 16:55:22 -0700

    onclick

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-$(".b1").click(() => $(location).attr('href','/gladHost.html'));
 
-$(".b2").click(() => $(location).attr('href','/sessionMax.html'));
+var host = 'gladHost.html';
+
+$(".b1").click(() => $(location).attr('href','https://jaceybuchner.github.io/GOC/'+host));
+
+var max = 'sessionMax.html';
+
+$(".b2").click(() => $(location).attr('href','https://jaceybuchner.github.io/GOC/'+max));
 
 $(".b3").click(() => alert("This session is closed")); 
