02e174c4113405b106fbbc3ae86153237dfc5b7b
Jacey Buchner <jjmb.private@gmail.com>
2020-07-22 16:48:08 -0700

    maximilus page / buttons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
-$("button").on("click", () =>{
-    $(this).hide();
-})
+$(".b1").click(() => $(location).attr('href','/gladHost.html'));
+
+$(".b2").click(() => $(location).attr('href','/sessionMax.html'));
+
+$(".b3").click(() => alert("This session is closed")); 
