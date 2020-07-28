c9a658fdf4ee2f1459a0ddfe4744403d7f7cbe7d
Jacey Buchner <jjmb.private@gmail.com>
2020-07-27 23:37:15 -0700

    sessionMaxv2/html styling

--- js/script.js
+++ js/script.js
@@ -3,8 +3,16 @@ var host = 'gladHost.html';
 
 $(".b1").click(() => $(location).attr('href','https://jaceybuchner.github.io/GOC/'+host));
 
-var max = 'sessionMax.html';
+var max = 'sessionMaxv2.html';
 
 $(".b2").click(() => $(location).attr('href','https://jaceybuchner.github.io/GOC/'+max));
 
 $(".b3").click(() => alert("This session is closed")); 
+
+var rolesA = 'rolesSpotA';
+
+var rolesB = 'rolesSpotB';
+
+$(".rolesA").click(() => alert("this will route you to the Roles A Assignment page"));
+
+$(".rolesB").click(() => alert("this will route you to the Roles B Assignment page"));
